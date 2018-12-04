d1589d91fb2c2541e9692ab772382282d536dd04
Tristan <tdb.work@outlook.fr>
2018-12-04 12:46:35 +0100

    Refactoring QLearning

--- module/algorithm/Matrix.java
+++ module/algorithm/Matrix.java
@@ -2,25 +2,26 @@ package COCOMA_RoboRescue.module.algorithm;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
-public class Matrix {
+import COCOMA_RoboRescue.module.algorithm.State;
+import COCOMA_RoboRescue.module.algorithm.Action;
+
+
+public class Q {
 
     //| =============================
     //| ========== MEMBERS ==========
     //| =============================
-    private int rows;
-    private int cols;
-    private float[][] values;
+    private HashMap<State, HashMap<Action, Float>> values;
 
 
     //| =======================================
     //| ==========  PUBLIC FUNCTIONS ==========
     //| =======================================
-    Matrix(int rows, int cols){
-        this.rows = rows;
-        this.cols = cols;
-        values = new float[rows][cols];
+    Q(){
+        values = new HashMap<State, HashMap<Action, Float>>();
     }
 
     public void fill(float value){
@@ -30,19 +31,16 @@ public class Matrix {
     }
 
     public void fillRow(int row, float value){
-        checkCoordinates(row, 0);
         Arrays.fill(values[row], value);
     }
 
     public void fillColumn(int col, float value){
-        checkCoordinates(0, col);
         for(float[] row: values){
             row[col] = value;
         }
     }
 
     public float getMaxFromRow(int row){
-        checkCoordinates(row, 0);
         float max = values[row][0];
         for(float value : values[row]){
             if (value > max)
@@ -52,8 +50,6 @@ public class Matrix {
     }
 
     public int getMaxIndexFromRow(int row){
-        checkCoordinates(row, 0);
-
         int     index   = 0;
         float   max     = values[row][index];
         float   value;
@@ -71,26 +67,11 @@ public class Matrix {
     //| =======================================
     //| ========== GETTERS & SETTERS ==========
     //| =======================================
-    public int getRows(){ return rows; }
-    public int getCols(){ return cols; }
-
     public float getValuetAt(int row, int col){
-        checkCoordinates(row, col);
         return values[row][col];
     }
 
     public void setValueAt(float value, int row, int col){
-        checkCoordinates(row, col);
         values[row][col] = value;
-    }
-
-
-    //| ========================================
-    //| ==========  PRIVATE FUNCTIONS ==========
-    //| ========================================
-    private void checkCoordinates(int x, int y){
-        if(x >= 0 && x < rows && y >= 0 && y < cols){
-            throw new IllegalArgumentException("Coordinate out of bounds");
-        }
     }
 }
